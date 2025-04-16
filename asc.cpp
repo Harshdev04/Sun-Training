8ec8095dd5cf4f62fa75e2cf2d2222226b53b85e
Harsh Dev <harsharora45642@gmail.com>
2025-04-16 12:47:23 +0530

    second Lecture

---
+++ asc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char harsh= 'a';
+   cout<<harsh<<endl;
+   int x = harsh;
+   cout<<x<<endl;
+    return 0;
+}
--- day1.cpp
+++ day1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" ;
+    cout << "Hello, World!" << endl;
+    int a;
+    a=90;
+    cout << a << endl;
     return 0;
-}
+} 
