7bb8d2e5b89e9cf3ae27b5587aab262f9185a349
Harsh Dev <harsharora45642@gmail.com>
2025-04-17 12:34:36 +0530

    third  Lecture

---
+++ day2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    // int a;
+    // a=30;
+    // int b;   //operator
+    // b=90;
+    //  b/=a;
+    // cout << b << endl;
+    // int n = 10;
+    // if(n==11){
+    //     cout << "n is 10" << endl;
+    //     cout << n*n*n << endl;       //conditonal statement 
+    //     cout << "Harsh" << endl;  
+    // }
+    // else{
+    //     cout << "n is not 10" << endl;
+    //     cout << n+n+n << endl;
+    //     cout << "Dev" << endl;
+    // }
+ 
+
+    // int n;
+    // cin >> n;
+    
+    // if (n%2 == 0){
+    //     cout << "n is even" << endl;
+    // }
+    // else{                                                //even or odd
+    //     cout << "n is odd" << endl;
+    //     }
+
+int a,b,c;
+cin>>a>>b>>c;
+if(a>b && a>c){
+    cout<<"a is greater"<<endl;
+    }else if(b>c && b>a){
+        cout<<"b is greater"<<endl;      //greater number 
+        }else{
+            cout<<"c is greater"<<endl;
+            }
+
+    return 0;
+}      
